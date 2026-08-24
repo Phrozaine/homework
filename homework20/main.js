b53443c9fb2148b1984406529bec86fc321ff71e
Phrozaine <roma.fdorov.06@bk.ru>
2026-08-24 20:04:44 +0300

    Update main.js

--- homework20/main.js
+++ homework20/main.js
@@ -5,8 +5,8 @@ var age = 17;
 var currentYear = 2026;
 var birthYear = currentYear - age;
 
-console.log(
-  `Меня зовут ${firstName} ${lastName}, мне ${age} лет. Я ученик курса: ${isStudent}. Я ${birthYear} года рождения`,
-);
+var text = `Меня зовут ${firstName} ${lastName}, мне ${age} лет. Я ученик курса: ${isStudent}. Я ${birthYear} года рождения`;
+
+document.getElementById("result").textContent = text;
 
 // Значение result 123456789falsetrue
