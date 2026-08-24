2cb3573d48815755b58bfba8f628188237a3c973
Phrozaine <roma.fdorov.06@bk.ru>
2026-08-24 19:44:51 +0300

    Add files via upload

---
+++ homework20/main.js
@@ -0,0 +1,12 @@
+var firstName = "Роман";
+var lastName = "Фёдоров";
+var isStudent = true;
+var age = 17;
+var currentYear = 2026;
+var birthYear = currentYear - age;
+
+console.log(
+  `Меня зовут ${firstName} ${lastName}, мне ${age} лет. Я ученик курса: ${isStudent}. Я ${birthYear} года рождения`,
+);
+
+// Значение result 123456789falsetrue
