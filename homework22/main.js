5feef79d179e137888c9afc945d7ca2853a1ae02
Phrozaine <roma.fdorov.06@bk.ru>
2026-08-26 20:15:50 +0300

    Update main.js

--- homework22/main.js
+++ homework22/main.js
@@ -4,8 +4,9 @@ switch (task) {
   case 1:
     for (i = 1; i <= 20; i++) {
       if (i % 4 == 0) {
-        console.log(i);
+        continue
       }
+      console.log(i);
     }
     break;
   case 2:
